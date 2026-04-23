98e88204f64be7db63456bc82796da29f86bbd8a
nguyentuanduong1004 <78344086+nguyentuanduong1004@users.noreply.github.com>
2026-04-23 14:04:54 +0700

    PrimeNG 19 components styles are having errors. Deep analyze and fix it

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err)
-);
+import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideAnimationsAsync(),
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
+  ],
+}).catch((err) => console.error(err));
